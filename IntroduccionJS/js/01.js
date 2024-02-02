4321ad857a60476ccfb53b39d6c07eefd04e0ee6
Nahuatlatofl <132028677+Nahuatlatofl@users.noreply.github.com>
2024-02-01 22:49:58 -0800

    programacion web

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,45 @@
+// Tipos De Variables
+
+var string = "Gamer Pods"; 
+var float = 1.092;
+var int = 1;
+var boolean = true;
+var _null = null;
+
+/*
+     Javascript es un lenguaje de tipo dinamico
+     eso quiere decir que no es necesario declarar el tipo 
+     de dato.
+ */
+
+/* 
+    Reglas al declarar variables:
+    No utilizar espacios.
+    Si quieres poner en el nombre de la variable números, hay que ponerlos a partir de la primera letra.
+    No poner la primera letra en mayúscula.
+    La variable si ha de empezar con una letra o guión bajo (_).
+    No utilizar más de 30 caracteres.
+    No utilizar palabras reservadas.
+*/
+
+/* Estilos para crear variables */
+
+var var_ejemplo = "XD" //Underscore
+var varEjemplo = "XD"  //Camelcase
+var VarEjemplo = "XD"  //Pascal Case
+var varejemplo = "XD"  //tonto case
+
+/* Operaciones aritmeticas */
+
+var n1 = 1, n2= 3;
+var plus = n1 + n2;
+var plus = n1 - n2;
+var plus = n1 * n2;
+var plus = n1 / n2;
+
+/* Concatenacion de strings */
+
+var string1 = "Hola ", string2 = "Mundo";
+var Concatenacion = string1 + string2;
+
+/* JavaScript posee sensibilidad a las Mayusculas */
---
+++ IntroduccionJS/js/02.js
@@ -0,0 +1,13 @@
+/* Declaracion De Variables */
+
+var soyUnaVariableDeclaradaConVar;
+let soyUnaVariableDeclaradaConLet;
+
+/* Constantes */
+
+const soyUnaConstante = 1;
+
+/* Las constantes son variables inicializadas que
+ no se pueden modificar a lo largo de la ejucion */
+
+ 
